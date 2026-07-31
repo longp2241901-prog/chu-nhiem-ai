178ee8cd21b62d2dab63b0de2c457f71eeaa9f1f
longp2241901-prog <longp2241901@gmail.com>
2026-07-31 15:04:35 +0700

    matrix_exam_app.py

--- matrix_exam_app.py
+++ matrix_exam_app.py
@@ -527,7 +527,7 @@ Yêu cầu:
 
             try:
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash-lite-lite",
+                    model="gemini-2.5-pro-lite",
                     contents=prompt,
                 )
                 output = response.text
@@ -578,7 +578,7 @@ Yêu cầu:
 """
             try:
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash-lite",
+                    model="gemini-2.5-pro",
                     contents=prompt,
                 )
                 output = response.text
@@ -609,7 +609,7 @@ Yêu cầu:
 """
             try:
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash-lite",
+                    model="gemini-2.5-pro",
                     contents=prompt,
                 )
                 output = response.text
